743b35c869abfe68af61ad6041c3aed00b617161
Matus Goljer <matus.goljer@gmail.com>
2017-05-24 12:36:19 +0200

    Update copyright

--- neon-mode.el
+++ neon-mode.el
@@ -1,6 +1,6 @@
 ;;; neon-mode.el --- Simple major mode for editing neon files
 
-;; Copyright (C) 2015 Matúš Goljer <matus.goljer@gmail.com>
+;; Copyright (C) 2015-2017 Matúš Goljer <matus.goljer@gmail.com>
 
 ;; Author: Matúš Goljer <matus.goljer@gmail.com>
 ;; Maintainer: Matúš Goljer <matus.goljer@gmail.com>
