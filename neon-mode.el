70407a7de6a6cfde9fdb6c1166582d7a0384264e
Matus Goljer <matus.goljer@gmail.com>
2017-05-24 13:51:58 +0200

    Add font-locking for enabled and disabled

--- neon-mode.el
+++ neon-mode.el
@@ -94,7 +94,8 @@ echo json_encode($result);"))
   `(
     (,(concat "\\<" (regexp-opt '("true" "True" "TRUE" "yes" "Yes"
                                   "YES" "on" "On" "ON" "false" "False"
-                                  "FALSE" "no" "No" "NO" "off" "Off" "OFF"))
+                                  "FALSE" "no" "No" "NO" "off" "Off" "OFF"
+                                  "enabled" "disabled"))
               "\\>")
      0 'font-lock-constant-face)
     ("\\<%\\(.*?\\)%\\>" 0 'font-lock-function-name-face)
